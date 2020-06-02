6425e298a9160307fb99b0e643daf4cd8e034bcd
Nicolas Teixeira <nicolasteixeira3856@outlook.com>
2020-06-02 15:41:10 -0300

    Commit da segunda aula, adicionado rotas de inserção e listagem de dados, com ou sem filtro, configuração no knex como ORM, conexão com o banco de dados SQLite, criação de migrations e seeds

--- readme.md
+++ readme.md
@@ -12,12 +12,22 @@
 - npx tsc --init,
 - npm install ts-node-dev -D,
 - npx ts-node-dev src/server.ts 
+- npm install knex
+- npx knex --knexfile knexfile.ts migrate:latest para a criação das migrations
+- npx knex --knexfile knexfile.ts seed:run para os seeds do banco de dados
+- npm install cors para a configuração de cors no app
 
-Adicionando também ao NodeJS o comando de dev no package.json da seguinte forma: 
+## Atenção para todos os scripts para execução adicionadas no package.json
+
+Adicionando também ao NodeJS o comando de dev, knex:migrate, knex:seed no package.json da seguinte forma: 
 
 ```
-"scripts": {
+  "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1",
-    "dev": "ts-node-dev src/server.ts "
-},
+    "dev": "ts-node-dev --transpileOnly --ignore-watch node_modules src/server.ts",
+    "knex:migrate": "knex --knexfile knexfile.ts migrate:latest",
+    "knex:seed": "knex --knexfile knexfile.ts seed:run"
+  },
 ```
+
+O programa Insomnia foi utilizado para teste das rotas da aplicação de back-end feita em NodeJS
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,61 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log("Listagem de usuários");
+app.use(cors())
 
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robison',
-        'Nicolas'
-    ]);
-})
+app.use(express.json());
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
+
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualizar uma informação existente no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST http://localhost:3333/users = Criar um usuário
+// GET http://localhost:3333/users = Listar usuários
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com ID 5
+
+// Request Param: Parametros que vem na própria rota que identificam um recurso
+// Query Param: Parametros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parametros para cração/atualização de informações
+
+// const users = [
+//     'Diego',
+//     'Cleiton',
+//     'Robison',
+//     'Nicolas'
+// ]
+
+// app.get('/users', (request, response) => {
+    // return response.json({})
+    // const search = String(request.query.search);
+    // const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    // return response.json(filteredUsers);
+// });
+
+// app.get('/users/:id', (request, response) =>{
+//     const id = Number(request.params.id);
+//     const user = users[id];
+//     return response.json(user);
+// });
+
+// app.post('/users', (request, response) => {
+//     const data = request.body;
+//     const user = {
+//         name: data.name,
+//         email: data.email
+//     };
+//     return response.json(user);
+// })
